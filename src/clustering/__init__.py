56c27350f0f530d671e45e17fa82c823b32208fa
Danendra Shafi Athallah <164164794+danenftyessir@users.noreply.github.com>
2026-05-19 18:33:01 +0700

    feat: add Ward hierarchical clustering on SNP distance matrix with dendrogram-based cluster assignment

---
+++ src/clustering/__init__.py
@@ -0,0 +1,5 @@
+from .hierarchical import hierarchical_clustering, assign_clusters
+from .reduction import run_pca, run_umap, run_tsne
+from .visualize import plot_dendrogram, plot_heatmap, plot_scatter
+from .validation import validate_clusters_vs_metadata, plot_cluster_composition
+from .metrics import compute_silhouette
